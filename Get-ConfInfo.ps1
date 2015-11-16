54600789654bcb37061c243c2ff4687368d925e1
brianbunke <brianbunke+github@gmail.com>
2015-11-15 21:39:05 -0800

    Initial commit

---
+++ Get-ConfInfo.ps1
@@ -0,0 +1,65 @@
+﻿function Get-ConfInfo {
+    <#
+    .SYNOPSIS
+    Gather URI/auth info for use in this session's REST API requests.
+
+    .DESCRIPTION
+    Anonymous access to the Confluence REST API is disabled by default.
+    Unless manually enabled, you will need to pass authorization in all interactions.
+
+    If you have a better suggestion for how to handle this, please reach out on GitHub!
+
+    .PARAMETER BaseURI
+    Address of your base Confluence install. For Atlassian On-Demand instances, include /wiki.
+
+    .PARAMETER Username
+    The username you use to log in to Confluence.
+
+    .PARAMETER Password
+    The password you use to log in to Confluence.
+
+    .EXAMPLE
+    Get-ConfInfo -BaseURI 'https://brianbunke.atlassian.net/wiki' -Username admin -Password uncrackable
+    Declare Confluence home address and credentials. Stored in script-scope variables $BaseURI and $Header
+    
+    .LINK
+    https://github.com/brianbunke/ConfluencePS
+
+    .LINK
+    http://stackoverflow.com/questions/27951561/use-invoke-webrequest-with-a-username-and-password-for-basic-authentication-on-t
+    #>
+	[CmdletBinding()]
+	param (
+	    [Parameter( Mandatory   = $true,
+                    HelpMessage = 'Example = https://brianbunke.atlassian.net/wiki (/wiki for On-Demand instances)')]
+	    [ValidateNotNullorEmpty()]
+        [Uri]$BaseURI = 'https://brianbunke.atlassian.net/wiki',
+
+	    [Parameter(Mandatory = $true)]
+	    [ValidateNotNullorEmpty()]
+        [string]$Username,
+
+        [Parameter(Mandatory = $true)]
+	    [ValidateNotNullorEmpty()]
+        [string]$Password
+    )
+
+    PROCESS {
+        # Append the /rest/api to our URI
+        # Save as script-level variable for further use in the current session
+        $script:BaseURI = $BaseURI.AbsoluteUri.TrimEnd('/') + '/rest/api'
+
+        $Pair = "${Username}:${Password}"
+
+        # Encode the string to the RFC2045-MIME variant of Base64, except not limited to 76 char/line.
+        $Bytes = [System.Text.Encoding]::ASCII.GetBytes($Pair)
+        $Base64 = [System.Convert]::ToBase64String($Bytes)
+
+        # Create the Auth value as the method, a space, and then the encoded pair Method Base64String
+        $BasicAuthValue = "Basic $Base64"
+
+        # Create the header hashtable to pass your basic auth
+        # Save as script-level variable for further use in the current session
+        $script:Header = @{ Authorization = $BasicAuthValue }
+    }
+}
---
+++ Get-ConfSpace.ps1
@@ -0,0 +1,64 @@
+﻿function Get-ConfSpace {
+    <#
+    .SYNOPSIS
+    Retrieve a listing of spaces in your Confluence instance.
+
+    .DESCRIPTION
+    Fetch all Confluence spaces, optionally filtering by Name/Key/ID.
+
+    Piped output into other cmdlets is generally tested and supported.
+
+    .PARAMETER Name
+    Filter results by name. Supports wildcard matching on partial names.
+
+    .PARAMETER Key
+    Filter results by key.
+
+    .PARAMETER ID
+    Filter results by ID.
+
+    .EXAMPLE
+    Get-ConfSpace -ID 123456
+    Display the info of the space with ID 123456.
+
+    .EXAMPLE
+    Get-ConfSpace -Name test
+    Display all spaces containing 'test' in the name.
+
+    .LINK
+    https://github.com/brianbunke/ConfluencePS
+    #>
+	[CmdletBinding()]
+	param (
+        [string]$Name,
+
+        [Alias('SpaceKey')]
+        [string]$Key,
+
+        [string]$ID
+    )
+
+    BEGIN {
+        If (!($Header) -or !($BaseURI)) {
+            Write-Verbose 'URI or authentication not found. Calling Get-ConfInfo'
+            Get-ConfInfo
+        }
+    }
+
+    PROCESS {
+        $URI = $BaseURI + '/space'
+
+        Write-Verbose "Fetching info from $URI"
+        $Rest = Invoke-RestMethod -Headers $Header -Uri $URI -Method Get | Select -ExpandProperty Results | Select Key,Name,ID,Type
+
+        If ($ID) {
+            $Rest | Where {$_.ID -eq $ID}
+        } ElseIf ($Key) {
+            $Rest | Where {$_.Key -eq $Key}
+        } ElseIf ($Name) {
+            $Rest | Where {$_.Name -like "*$Name*"}
+        } Else {
+            $Rest
+        }
+    }
+}
