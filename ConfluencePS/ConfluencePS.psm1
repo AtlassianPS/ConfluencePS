eeafbf31c5f923b60db1e46e3f893d6c5226aa08
brianbunke <brianbunke+github@gmail.com>
2015-11-22 13:43:16 -0800

    Module creation

---
+++ ConfluencePS/ConfluencePS.psm1
@@ -0,0 +1,14 @@
+﻿# Gather all files
+$PublicFunctions = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
+# $PrivateFunctions = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+
+# Dot source the functions
+ForEach ($File in $PublicFunctions) {
+    Try {
+        . $File.FullName
+    } Catch {
+        Write-Error -Message "Failed to import function $($File.FullName): $_"
+    }
+}
+
+Export-ModuleMember -Function $PublicFunctions.Basename
