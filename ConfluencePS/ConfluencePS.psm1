4975f644027c94217795f03985c47eb06f3d0aab
brianbunke <brianbunke+github@gmail.com>
2015-11-28 14:38:14 -0800

    Future-proofing any private functions

--- ConfluencePS/ConfluencePS.psm1
+++ ConfluencePS/ConfluencePS.psm1
@@ -1,9 +1,10 @@
 ﻿# Gather all files
 $PublicFunctions = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
-# $PrivateFunctions = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+### $PrivateFunctions = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
 # Dot source the functions
 ForEach ($File in $PublicFunctions) {
+### ForEach ($File in @($PublicFunctions + $PrivateFunctions)) {
     Try {
         . $File.FullName
     } Catch {
