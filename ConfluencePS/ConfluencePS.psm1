667ba1464cfcd83cf0461317494537f1eec55356
Oliver Lipkau <oliver@lipkau.net>
2017-03-19 21:19:28 +0100

    Load Private functions

--- ConfluencePS/ConfluencePS.psm1
+++ ConfluencePS/ConfluencePS.psm1
@@ -1,13 +1,13 @@
 ﻿# Gather all files
 $PublicFunctions = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
-### $PrivateFunctions = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
+$PrivateFunctions = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 
 # Dot source the functions
-ForEach ($File in $PublicFunctions) {
-### ForEach ($File in @($PublicFunctions + $PrivateFunctions)) {
+ForEach ($File in @($PublicFunctions + $PrivateFunctions)) {
     Try {
         . $File.FullName
-    } Catch {
+    }
+    Catch {
         Write-Error -Message "Failed to import function $($File.FullName): $_"
     }
 }
