574b4cda43a45e7b0e29f1e6c40ffcc07fca0342
Oliver Lipkau <oliver@lipkau.net>
2017-03-23 20:06:00 +0100

    Implemented Space class

---
+++ ConfluencePS/ConfluencePS.Types.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Linq;
+
+namespace ConfluencePS
+{
+
+	public class CIcon {
+		public String Path { get; set; }
+		public Int32 Width { get; set; }
+		public Int32 Height { get; set; }
+		public Boolean IsDefault { get; set; }
+	}
+
+	public class Space {
+		private String _description;
+
+		public Int32 Id { get; set; }
+		public String Key { get; set; }
+		public String Name { get; set; }
+		public CIcon Icon { get; set; }
+		public String Type { get; set; }
+		public dynamic Description {
+			set {
+				try {
+					dynamic intermediateValue = value.plain;
+					_description = intermediateValue.value.ToString();
+					return;
+				} catch {
+					var dict = value as IDictionary;
+					if(dict != null && dict.Contains("plain")) {
+						_description = dict["plain"].ToString();
+					} else {
+						_description = value.ToString();
+					}
+				}
+			}
+			get {
+				return _description;
+			}
+		}
+	}
+}
