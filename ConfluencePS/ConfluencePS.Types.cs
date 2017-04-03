f33995b4982a444fc47b705e3b2b5be34969dd76
Oliver Lipkau <oliver@lipkau.net>
2017-04-03 17:42:38 +0200

    updated classes for the private conversion functions

--- ConfluencePS/ConfluencePS.Types.cs
+++ ConfluencePS/ConfluencePS.Types.cs
@@ -13,78 +13,39 @@ namespace ConfluencePS
 		public Boolean IsDefault { get; set; }
 	}
 
-	public class Author {
+	public class User {
 		public String UserName { get; set; }
 		public String DisplayName { get; set; }
-		public String Key { get; set; }
-		public Icon Picture { get; set; }
+		public String UserKey { get; set; }
+		public Icon ProfilePicture { get; set; }
 	}
 
 	public class Version {
-		public Author By { get; set; }
-		public DateTime Author { get; set; }
+		public User By { get; set; }
+		public DateTime When { get; set; }
+		public String FriendlyWhen { get; set; }
 		public Int32 Number { get; set; }
 		public String Message { get; set; }
 		public Boolean MinorEdit { get; set; }
 	}
 
 	public class Space {
-		private String _description;
-
 		public Int32 Id { get; set; }
 		public String Key { get; set; }
 		public String Name { get; set; }
 		public Icon Icon { get; set; }
 		public String Type { get; set; }
+		public String Description { get; set; }
 		// TODO: homepage ?
-		public dynamic Description {
-			set {
-				try {
-					dynamic intermediateValue = value.plain;
-					_description = intermediateValue.value.ToString();
-					return;
-				} catch {
-					var dict = value as IDictionary;
-					if(dict != null && dict.Contains("plain")) {
-						_description = ((Hashtable)dict["plain"])["value"].ToString();
-					} else {
-						_description = value.ToString();
-					}
-				}
-			}
-			get {
-				return _description;
-			}
-		}
 	}
 
 	public class Page {
-		private String _description;
-
 		public Int32 ID { get; set; }
 		public String Status { get; set; }
 		public String Title { get; set; }
 		public Space Space { get; set; }
 		public Version Version { get; set; }
-		public dynamic Body {
-			set {
-				try {
-					dynamic intermediateValue = value.storage;
-					_description = intermediateValue.value.ToString();
-					return;
-				} catch {
-					var dict = value as IDictionary;
-					if(dict != null && dict.Contains("storage")) {
-						_description = ((Hashtable)dict["storage"])["value"].ToString();
-					} else {
-						_description = value.ToString();
-					}
-				}
-			}
-			get {
-				return _description;
-			}
-		}
+		public String Body { get; set; }
 		public Page[] Ancestors { get; set; }
 		// TODO: URL?
 	}
