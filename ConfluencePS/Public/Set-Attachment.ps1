ae1d2ab511d9394a02ef3f30b8337e07a216cb45
John Adcock <adcockj@users.sourceforge.net>
2018-02-12 08:30:00 +0000

    Add initial version of Set-Attachment

---
+++ ConfluencePS/Public/Set-Attachment.ps1
@@ -0,0 +1,102 @@
+function Set-Attachment {
+    [CmdletBinding(
+        ConfirmImpact = 'Low',
+        SupportsShouldProcess = $true
+    )]
+    [OutputType([ConfluencePS.Attachment])]
+    param (
+        [Parameter( Mandatory = $true )]
+        [URi]$apiURi,
+
+        [Parameter( Mandatory = $true )]
+        [PSCredential]$Credential,
+
+        [Parameter(
+            Mandatory = $true,
+            ValueFromPipeline = $true
+        )]
+        [ConfluencePS.Attachment]$Attachment,
+
+        # Path of the file to upload and attach
+        [Parameter( Mandatory)]
+        [ValidateScript(
+
+            {
+                if (-not (Test-Path $_ -PathType Leaf)) {
+                    $errorItem = [System.Management.Automation.ErrorRecord]::new(
+                        ([System.ArgumentException]"File not found"),
+                        'ParameterValue.FileNotFound',
+                        [System.Management.Automation.ErrorCategory]::ObjectNotFound,
+                        $_
+                    )
+                    $errorItem.ErrorDetails = "No file could be found with the provided path '$_'."
+                    $PSCmdlet.ThrowTerminatingError($errorItem)
+                }
+                else {
+                    return $true
+                }
+            }
+        )]
+        [String]$FilePath
+    )
+
+    BEGIN {
+        Write-Verbose "[$($MyInvocation.MyCommand.Name)] Function started"
+
+        $resourceApi = "$apiURi/content/{0}/child/attachment/{1}/data"
+    }
+
+    PROCESS {
+        Write-Debug "[$($MyInvocation.MyCommand.Name)] ParameterSetName: $($PsCmdlet.ParameterSetName)"
+        Write-Debug "[$($MyInvocation.MyCommand.Name)] PSBoundParameters: $($PSBoundParameters | Out-String)"
+
+        $iwParameters = @{
+            Uri        = ""
+            Method     = 'Post'
+            Body       = ""
+            OutputType = [ConfluencePS.Attachment]
+            Credential = $Credential
+        }
+
+        foreach ($_attachment in $Attachment) {
+            $readFile = Get-Content -Path $FilePath -Encoding Byte
+            $enc = [System.Text.Encoding]::GetEncoding("iso-8859-1")
+            $fileEnc = $enc.GetString($readFile)
+            $boundary = [System.Guid]::NewGuid().ToString()
+            $bodyLines = @'
+--{0}
+Content-Disposition: form-data; name="file"; filename="{1}"
+Content-Type: application/octet-stream
+
+{2}
+--{0}--
+
+'@ -f $boundary, $_attachment.Title, $fileEnc
+
+            $headers = @{
+                'X-Atlassian-Token' = 'nocheck'
+                'Content-Type'      = "multipart/form-data; boundary=$boundary"
+            }
+
+            $parameter = @{
+                URI        = $resourceApi -f $_attachment.PageID, $_attachment.ID
+                Method     = "POST"
+                Body       = $bodyLines
+                Headers    = $headers
+                RawBody    = $true
+                Credential = $Credential
+                OutputType = [ConfluencePS.Attachment]
+                Verbose    = $false
+            }
+            Write-Debug "[$($MyInvocation.MyCommand.Name)] Invoking Set Attachment Method with `$parameter"
+            if ($PSCmdlet.ShouldProcess($_page, "Updating attachment '$($_attachment.Title)'.")) {
+                Invoke-Method @parameter
+            }
+            
+        }
+    }
+
+    END {
+        Write-Verbose "[$($MyInvocation.MyCommand.Name)] Function ended"
+    }
+}
