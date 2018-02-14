e01deeab81196255c634dce209d7d437cc32f0b7
Oliver Lipkau <oliver@lipkau.net>
2018-02-14 19:26:45 +0100

    Reverted changes to Integration Tests

--- Tests/ConfluencePS.Integration.Tests.ps1
+++ Tests/ConfluencePS.Integration.Tests.ps1
@@ -838,7 +838,7 @@ InModuleScope ConfluencePS {
         "PESTER1" | Remove-ConfluenceSpace -Force -ErrorAction Stop
 
         # ASSERT
-        Start-Sleep -Seconds 10
+        Start-Sleep -Seconds 1
         It 'space is no longer available' {
             { Get-ConfluenceSpace -Key PESTER -ErrorAction Stop } | Should Throw
             { Get-ConfluenceSpace -Key PESTER1 -ErrorAction Stop } | Should Throw
