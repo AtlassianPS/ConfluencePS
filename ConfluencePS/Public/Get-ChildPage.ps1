3bb123f56a320c8f840eedab369791a0c5744fff
ritzcrackr <ritzcrackr@outlook.com>
2019-02-10 16:18:04 -0500

    Add Support for Certificate and Annoymous Auth to Get-ChildPage
    Get-ChildPage.ps1
    -Added Certificate Parameter
    -Made Credential Parameter not mandatory
    -Rename $ApiUri to have proper casing.
    -Simplified the propagating of the the splatted parameters.
    -Simplified the "Recurse" logic.
    -Identified issue handling ConfluencePS.Page objects via pipeline.
    
    Get-ChildPage.md
    -Added Certificate Parameter
    -Made Credential Parameter not mandatory
    
    Get-Page.md
    -Fixed 'ApiUri' casing in documentation

--- ConfluencePS/Public/Get-ChildPage.ps1
+++ ConfluencePS/Public/Get-ChildPage.ps1
@@ -3,10 +3,15 @@ function Get-ChildPage {
     [OutputType([ConfluencePS.Page])]
     param (
         [Parameter( Mandatory = $true )]
-        [URi]$apiURi,
+        [URi]$ApiUri,
 
-        [Parameter( Mandatory = $true )]
+        [Parameter( Mandatory = $false )]
         [PSCredential]$Credential,
+
+        [Parameter( Mandatory = $false )]
+        [ValidateNotNull()]
+        [System.Security.Cryptography.X509Certificates.X509Certificate]
+        $Certificate,
 
         [Parameter(
             Position = 0,
@@ -26,35 +31,35 @@ function Get-ChildPage {
 
     BEGIN {
         Write-Verbose "[$($MyInvocation.MyCommand.Name)] Function started"
-
-        $depthLevel = "child"
     }
 
     PROCESS {
         Write-Debug "[$($MyInvocation.MyCommand.Name)] ParameterSetName: $($PsCmdlet.ParameterSetName)"
         Write-Debug "[$($MyInvocation.MyCommand.Name)] PSBoundParameters: $($PSBoundParameters | Out-String)"
 
+        #Fix: See fix statement below. These two fix statements are tied together
         if (($_) -and -not($_ -is [ConfluencePS.Page] -or $_ -is [int])) {
             $message = "The Object in the pipe is not a Page."
             $exception = New-Object -TypeName System.ArgumentException -ArgumentList $message
             Throw $exception
         }
 
+        #Fix: This doesn't get called since there are no parameter sets for this function. It must be
+        #copy paste from another function. This function doesn't really accept ConfluencePS.Page objects, it only
+        #works due to powershell grabbing the 'ID' from ConfluencePS.Page using the
+        #'ValueFromPipelineByPropertyName = $true' and '[Alias('ID')]' on the PageID Parameter.
         if ($PsCmdlet.ParameterSetName -eq "byObject") {
             $PageID = $InputObject.ID
         }
-        if ($Recurse) { $depthLevel = "descendant" } # depth = ALL
 
-        $iwParameters = @{
-            Uri           = "$apiURi/content/{0}/{1}/page" -f $PageID, $depthLevel
-            Method        = 'Get'
-            GetParameters = @{
-                expand = "space,version,body.storage,ancestors"
-                limit  = $PageSize
-            }
-            OutputType    = [ConfluencePS.Page]
-            Credential    = $Credential
-        }
+        $iwParameters = Copy-CommonParameter -InputObject $PSBoundParameters
+        $iwParameters['Uri']           = if ($Recurse.IsPresent) {"$ApiUri/content/{0}/descendant/page" -f $PageID} else {"$ApiUri/content/{0}/child/page" -f $PageID}
+        $iwParameters['Method']        = 'Get'
+        $iwParameters['GetParameters'] = @{
+                                            expand = "space,version,body.storage,ancestors"
+                                            limit  = $PageSize
+                                         }
+        $iwParameters['OutputType']    = [ConfluencePS.Page]
 
         # Paging
         ($PSCmdlet.PagingParameters | Get-Member -MemberType Property).Name | ForEach-Object {
