d384acdf71d13e4eb70f36b4cd1e70f6501b0a61
ritzcrackr <ritzcrackr@outlook.com>
2019-02-10 22:12:21 -0500

    Add Support for Certificate and Annoymous Auth to ConvertTo-StorageFormat
    ConvertTo-StorageFormat.ps1
    -Added Certificate Parameter
    -Made Credential Parameter not mandatory
    -Rename $ApiUri to have proper casing.
    -Simplified the propagating of the the splatted parameters.
    
    ConvertTo-StorageFormat.md
    -Added Certificate Parameter
    -Made Credential Parameter not mandatory

--- ConfluencePS/Public/ConvertTo-StorageFormat.ps1
+++ ConfluencePS/Public/ConvertTo-StorageFormat.ps1
@@ -3,10 +3,15 @@ function ConvertTo-StorageFormat {
     [OutputType([String])]
     param (
         [Parameter( Mandatory = $true )]
-        [URi]$apiURi,
+        [uri]$ApiUri,
 
-        [Parameter( Mandatory = $true )]
+        [Parameter( Mandatory = $false )]
         [PSCredential]$Credential,
+
+        [Parameter( Mandatory = $false )]
+        [ValidateNotNull()]
+        [System.Security.Cryptography.X509Certificates.X509Certificate]
+        $Certificate,
 
         [Parameter(
             Position = 0,
@@ -24,16 +29,15 @@ function ConvertTo-StorageFormat {
         Write-Debug "[$($MyInvocation.MyCommand.Name)] ParameterSetName: $($PsCmdlet.ParameterSetName)"
         Write-Debug "[$($MyInvocation.MyCommand.Name)] PSBoundParameters: $($PSBoundParameters | Out-String)"
 
+        $iwParameters = Copy-CommonParameter -InputObject $PSBoundParameters
+        $iwParameters['Uri']           = "$ApiUri/contentbody/convert/storage"
+        $iwParameters['Method']        = 'Post'
+
         foreach ($_content in $Content) {
-            $iwParameters = @{
-                Uri        = "$apiURi/contentbody/convert/storage"
-                Method     = 'Post'
-                Body       = @{
-                    value          = "$_content"
-                    representation = 'wiki'
-                } | ConvertTo-Json
-                Credential = $Credential
-            }
+            $iwParameters['Body'] = @{
+                                        value          = "$_content"
+                                        representation = 'wiki'
+                                    } | ConvertTo-Json
 
             Write-Debug "[$($MyInvocation.MyCommand.Name)] Content to be sent: $($_content | Out-String)"
             (Invoke-Method @iwParameters).value
